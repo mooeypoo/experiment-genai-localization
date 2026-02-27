adbbaa903d57870bd6d38eefa3ebd1362fbf1258
mooeypoo <moriel@gmail.com>
2026-02-26 21:57:59 -0500

    step-10: Locale-aware user sorting

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,17 +1,23 @@
 <script setup>
+import { computed } from 'vue'
 import { getUsers } from '../data/store.js'
-import { t } from '../i18n/index.js'
+import { t, locale } from '../i18n/index.js'
 import UserCard from '../components/UserCard.vue'
 import CreateUserForm from '../components/CreateUserForm.vue'
 
 const users = getUsers()
+
+const sortedUsers = computed(() => {
+  const collator = new Intl.Collator(locale.value, { sensitivity: 'base' })
+  return [...users].sort((a, b) => collator.compare(a.name, b.name))
+})
 </script>
 
 <template>
   <section class="users-view">
     <h1>{{ t('users.title') }}</h1>
     <div class="user-grid">
-      <UserCard v-for="user in users" :key="user.id" :user="user" />
+      <UserCard v-for="user in sortedUsers" :key="user.id" :user="user" />
     </div>
 
     <hr />
